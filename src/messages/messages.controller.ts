c7f8538d521c6c21366aca2f6f479f10aa25423f
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 01:01:27 +0300

    123

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get() // decorators here saves us from typing everything.
+  listMessages() {}
+  @Get('/:id') // this one works only with wildcard
+  getMessage() {}
+  @Post()
+  createMessage() {}
+}
