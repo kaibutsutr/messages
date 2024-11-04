423a5b6a80f2121f149a3cfe1c83547e08db65ef
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 02:09:14 +0300

    123123312

--- src/messages/dtos/create-message.dto.ts
+++ src/messages/dtos/create-message.dto.ts
@@ -1,5 +1,5 @@
 import { IsString } from 'class-validator'; // isString is a prebuilt validator that checks for strings
 export class CreateMessageDto {
-  @IsString // check the next line if its string or not
-  content: string;
+  @IsString()
+  content: string; // check the next line if its string or not
 }
