04713a132677d33db409605f761f2dc35f691722
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 01:23:30 +0300

    rest

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { log } from 'console';
 
 @Controller('messages')
 export class MessagesController {
   @Get() // decorators here saves us from typing everything.
   listMessages() {}
   @Get('/:id') // this one works only with wildcard
-  getMessage() {}
+  getMessage(@Param('id') id: string) {
+    // get id value from Param decorator and assign to id variable
+    console.log(id);
+  }
   @Post()
-  createMessage() {}
+  createMessage(@Body body: any) {
+    console.log(body);
+  }
 }
