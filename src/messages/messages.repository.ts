2cd53ad1b952ea437820d3e5703e5abdd0e2b411
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 21:18:50 +0300

    repoend

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -3,10 +3,21 @@ export class MessagesRepository {
   // we always create a class in nest
   async findOne(id: string) {
     // we use async function for CRUD
-    const contents = await readFile('messages.json', 'utf8');
-    const messages = JSON.parse(contents);
+    const contents = await readFile('messages.json', 'utf8'); // read the file messages.json and assign to object
+    const messages = JSON.parse(contents); // then parse it from json to object and send it back
     return messages[id];
   }
-  async findAll() {}
-  async createOne(message: string) {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8'); //open up the file
+    const messages = JSON.parse(contents); // read the contents and assign to objest
+    return messages; return the object
+  }
+  async createOne(content: string) {
+    const contents = await readFile('messages.json', 'utf8'); //open up the file
+    const messages = JSON.parse(contents); // read the contents and assign to objest
+    const id =Math.floor(Math.random(10000)) // pick a random id
+    messages.id={id, content:content};  // messages.id={id, content}; same thing. Write the message in object
+    await writeFile("messages.json",JSON.stringify(messages)); // update the new messages file to messages.json
+
+  }
 }
