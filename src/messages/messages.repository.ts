23de25689d61669760ecd717e5754448ead34e6e
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 22:16:58 +0300

    12323

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -16,8 +16,8 @@ export class MessagesRepository {
   async createOne(content: string) {
     const contents = await readFile('messages.json', 'utf8'); //open up the file
     const messages = JSON.parse(contents); // read the contents and assign to objest
-    const id = Math.floor(Math.random() * 1000); // pick a random id
-    messages.id = { id, content: content }; // messages.id={id, content}; same thing. Write the message in object
+    const id = Math.floor(Math.random() * 999); // pick a random id
+    messages.id = { id, content }; // messages.id={id, content}; same thing. Write the message in object
     await writeFile('messages.json', JSON.stringify(messages)); // update the new messages file to messages.json
   }
 }
