107d76f70b2edbfb7dca122fffd07ff97d729249
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 21:07:55 +0300

    readfile

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,1 +1,12 @@
-
+import { readFile, writeFile } from 'fs/promises'; // we need these from node to read write
+export class MessagesRepository {
+  // we always create a class in nest
+  async findOne(id: string) {
+    // we use async function for CRUD
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+  async findAll() {}
+  async createOne(message: string) {}
+}
